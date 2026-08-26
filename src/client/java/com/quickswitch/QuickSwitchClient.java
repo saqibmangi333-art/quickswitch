a60a7f5326e82460b0c5d582e41b9b1ad26dd1b0
saqibmangi333-art <saqibmangi333@gmail.com>
2026-08-26 18:19:56 +0500

    Create QuickSwitchClient.java

---
+++ src/client/java/com/quickswitch/QuickSwitchClient.java
@@ -0,0 +1,9 @@
+package com.quickswitch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuickSwitchClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
